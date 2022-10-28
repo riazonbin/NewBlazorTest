8b557462f04dd66618fb5d6bebba1ed7c55dcb6a
riazonbin <riazonbin@mail.ru>
2022-10-28 17:30:29 +0300

    working photo page

--- NewBlazorTest/Services/FileSystemService.cs
+++ NewBlazorTest/Services/FileSystemService.cs
@@ -21,24 +21,79 @@ namespace NewBlazorTest.Services
                 gridFS.UploadFromStream("ball.jpg", fs);
             }
         }
-
-        public void DownloadToLocal()
+        public void UploadImageToDb(string fileName, string path)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("ZaripovImages");
             var gridFS = new GridFSBucket(database);
-            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/Images/")}DeserializedBall.jpg", FileMode.CreateNew))
+
+            using (FileStream fs = new FileStream(path, FileMode.Open))
             {
-                gridFS.DownloadToStreamByName("deserializedBall.jpg", fs);
+                gridFS.UploadFromStream(fileName, fs);
             }
         }
-        public List<GridFSFileInfo> FindFileAsync()
+
+        public void DownloadFileToProject(GridFSFileInfo file)
         {
             var client = new MongoClient("mongodb://localhost");
             var database = client.GetDatabase("ZaripovImages");
-            IGridFSBucket gridFS = new GridFSBucket(database);
+            var gridFS = new GridFSBucket(database);
+            using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/wwwroot/images/")}{file.Filename}", FileMode.CreateNew))
+            {
+                gridFS.DownloadToStreamByName(file.Filename, fs);
+            }
+        }
+
+        public void GetImagesToProjectFolder()
+        {
+            AddImagesToDb();
+
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ZaripovImages");
+            var gridFS = new GridFSBucket(database);
+            var images = gridFS.Find(new BsonDocument()).ToList();
+            foreach (var image in images)
+            {
+                using (FileStream fs = new FileStream($"{Directory.CreateDirectory(Directory.GetCurrentDirectory() + "/wwwroot/images/")}{image.Filename}", FileMode.OpenOrCreate))
+                {
+                    gridFS.DownloadToStreamByName(image.Filename, fs);
+                }
+            }
+        }
+
+        public List<string> FindFiles()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ZaripovImages");
+            var gridFS = new GridFSBucket(database);
             var fileInfos = gridFS.Find(new BsonDocument()).ToList();
-            return fileInfos;
+            var imageInfos = new List<string>();
+            foreach(var file in fileInfos)
+            {
+                imageInfos.Add(file.Filename);
+            }
+            return imageInfos;
+        }
+
+        public bool FileExists(string filename)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ZaripovImages");
+            var gridFS = new GridFSBucket(database);
+
+            return gridFS.Find(FilterDefinition<GridFSFileInfo>.Empty).ToEnumerable().Any(x => x.Filename == filename);
+        }
+
+        private void AddImagesToDb()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("ZaripovImages");
+            var gridFS = new GridFSBucket(database);
+            foreach (var file in Directory.GetFiles("wwwroot/images"))
+            {
+                if (!FileExists(Path.GetFileName(file)))
+                    UploadImageToDb(Path.GetFileName(file), file);
+            }
         }
     }
 }
